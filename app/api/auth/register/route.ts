33e6d15027cb55b8a6d045eba1897a676ee57e2f
Roni Sore <ronisore360@gmail.com>
2026-02-16 00:04:10 +0800

    hash passwords when register

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,16 +1,31 @@
 import { connectDB } from "@/app/lib/mongodb";
 import User from "@/app/model/User";
-import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req: Request) {
+const createToken = (_id: mongoose.Types.ObjectId) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET!, { expiresIn: "3d" });
+};
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     await connectDB();
     const { email, password } = await req.json();
 
     const user = await User.signup({ email, password });
+    const token = createToken(user._id);
 
-    return NextResponse.json({ user }, { status: 201 });
-  } catch (err: any) {
-    return NextResponse.json({ error: err.massage }, { status: 404 });
+    const res = NextResponse.json({ user }, { status: 201 });
+    res.cookies.set("jwt", token, {
+      // set cookies
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "lax",
+    });
+    return res;
+  } catch (err) {
+    const message = err instanceof Error ? err.message : "Registration failed";
+    return NextResponse.json({ error: message }, { status: 400 });
   }
 }
--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
+import path from "path";
+import { config } from "dotenv";
 
-const MONGODB_URI = "mongodb://localhost:27017/NoteApp";
+config({ path: path.join(process.cwd(), "app", ".env") });
+
+const MONGODB_URI = process.env.MONGODB_URI ?? "mongodb://localhost:27017/NoteApp";
 
 export const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) {
